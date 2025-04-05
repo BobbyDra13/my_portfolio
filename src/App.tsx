3f4937c514204a3c3d3654e78786281c7656967d
BobbyDra13 <manaschandra07@gmail.com>
2025-04-05 17:36:01 +0530

    errors removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-import { Sun, Moon } from "lucide-react";
 import About from "./components/About";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
@@ -8,29 +6,9 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [darkMode]);
-
   return (
     <div className="bg-starry-night min-h-screen">
       <div className="max-w-5xl mx-auto px-6 py-8">
-        <header className="flex justify-end">
-          {/* <button onClick={() => setDarkMode(!darkMode)} className="p-2">
-            {darkMode ? (
-              <Sun className="w-6 h-6" />
-            ) : (
-              <Moon className="w-6 h-6" />
-            )}
-          </button> */}
-        </header>
-        {/* Your Components */}
         <Hero />
         <About />
         <Education />
